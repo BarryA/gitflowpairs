8da1a04045f1bfa8464529db957c21a0d553e717
Sam Puttman <daputter2@gmail.com>
2023-10-26 12:47:50 -0400

    added friend functionality

--- filename.rb
+++ filename.rb
@@ -1,1 +1,12 @@
-#Hello Gitflow Friends!
+class Gitflow_Friends
+
+  attr_reader :friend
+
+  def initialize
+    @friend_group_members = []
+  end
+
+  def add_friend(friend)
+    @friend_group_members << friend
+  end
+end
